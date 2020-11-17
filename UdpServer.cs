35fd15e871648ebeb0b223e32b523c3cd1e9cb47
ZackOrs <zachary_orsoli@hotmail.com>
2020-11-17 00:32:28 -0500

    Small changes

--- UdpServer.cs
+++ UdpServer.cs
@@ -17,22 +17,22 @@ namespace UDPSocketProject
         public ClientEventHandler clientTest = new ClientEventHandler();
 
         //these attributes are unique to each server
-        protected UdpClient serverSocket;//protected socket element
-        protected IPEndPoint ip;//the ip of the server
-        protected byte[] data;//the databus of the server
-        protected static int initServer = 0;//a static element that is incremented the moment a server is initalized
-        protected int personalID = 0;//
-        protected Stopwatch stopwatch = new Stopwatch();
-        protected Stopwatch notifyStopwatch = new Stopwatch();
+        //protected UdpClient serverSocket;//protected socket element
+        //protected IPEndPoint ip;//the ip of the server
+        //protected byte[] data;//the databus of the server
+        //protected static int initServer = 0;//a static element that is incremented the moment a server is initalized
+        //protected int personalID = 0;//
+        //protected Stopwatch stopwatch = new Stopwatch();
+        //protected Stopwatch notifyStopwatch = new Stopwatch();
         
-        public static bool allowReceive = true;
+        //public static bool allowReceive = true;
 
-        //shared information between servers
-        protected static int[] ports = { 8080, 5080 };//retains the ports of the servers
-        protected static string[] hosts = { "127.0.0.2", "127.0.0.2" };//retains the hosts of the servers
-        public static Semaphore semaphore = new Semaphore(1, 1, "Originate");
-        public Semaphore internalSemaphore = new Semaphore(1, 1);
-        public bool running = false;
+        ////shared information between servers
+        //protected static int[] ports = { 8080, 5080 };//retains the ports of the servers
+        //protected static string[] hosts = { "127.0.0.2", "127.0.0.2" };//retains the hosts of the servers
+        //public static Semaphore semaphore = new Semaphore(1, 1, "Originate");
+        //public Semaphore internalSemaphore = new Semaphore(1, 1);
+        //public bool running = false;
 
 
 
@@ -65,18 +65,6 @@ namespace UDPSocketProject
         }
 
         /// <summary>
-        /// Binds the ip to the socket and sets the timeout values for Send and Receive
-        /// </summary>
-        /// <param name="endPoint"></param>
-        public void BindSocket(IPEndPoint endPoint)
-        {
-            serverSocket = new UdpClient(endPoint);
-            serverSocket.Client.ReceiveTimeout = 5000;
-            serverSocket.Client.SendTimeout = 5000;
-        }
-
-
-        /// <summary>
         /// The primary execution of the server occurs in this function. The outer loop is blocked by a semaphore to prevent concurrent thread executions and the inner loop is where the server attempts to receive and send data
         /// </summary>
         public void Start()
@@ -104,8 +92,8 @@ namespace UDPSocketProject
 
                 string[] arr = receivedMessage.Split(",");
 
-                Console.WriteLine("Server {0} : {1}", personalID, receivedMessage);
-                string LogMessage = "String " + receivedMessage + " has been received from " + personalID.ToString();
+                Console.WriteLine("Server {0} : {1}", thisServerIP, receivedMessage);
+                string LogMessage = "String " + receivedMessage + " has been received from " + thisServerIP.ToString();
 
                 string serverResponse = clientTest.SwitchCase(receivedMessage, thisServerSocket);
                 byte[] feed = Encoding.ASCII.GetBytes(serverResponse);
@@ -113,31 +101,6 @@ namespace UDPSocketProject
 
                 //thisServerSocket.Send(feed, feed.Length, senderRemote);
                 thisServerSocket.SendTo(feed, 0, feed.Length,SocketFlags.None,(IPEndPoint)senderRemote);
-            }
-        }
-
-        [ObsoleteAttribute("Busywait function is obsolete. Functionality is implemented in Start function.", false)]
-        public void BusyWait()
-        {
-            Thread.Sleep(300000);
-        }
-        /// <summary>
-        /// Computes an individual action block on a time-based limit. Failure to complete the action in time forces the block to exit
-        /// </summary>
-        /// <param name="timeSpan"></param>
-        /// <param name="codeBlock"></param>
-        /// <returns></returns>
-        private static bool TrackFunction(TimeSpan timeSpan, Action codeBlock)
-        {
-            try
-            {
-                Task task = Task.Factory.StartNew(() => codeBlock());
-                task.Wait(timeSpan);
-                return task.IsCompleted;
-            }
-            catch (AggregateException ae)
-            {
-                throw ae.InnerExceptions[0];
             }
         }
 
